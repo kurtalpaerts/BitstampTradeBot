7ca65973900d47a950893e815983321775335f6d
Kurt Alpaerts <kurt.alpaerts@outlook.com>
2018-02-10 16:41:36 +0100

    Code move

---
+++ src/BitstampTradeBot.Exchange/Services/ApiKeys.cs
@@ -0,0 +1,9 @@
+﻿namespace BitstampTradeBot.Exchange.Services
+{
+    public static class ApiKeys
+    {
+        public const string BitstampApiKey = "YOUR API KEY";
+        public const string BitstampApiSecret = "YOUR API SECRET";
+        public const string BitstampCustomerId = "YOUR CUSTOMER ID";
+    }
+}
